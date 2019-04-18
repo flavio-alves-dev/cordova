6f5212287354f1d6724f0f52a9a2d259eab85a92
Flavio Alves <flavio.sisinfo@gmail.com>
2019-04-18 00:21:26 -0300

    primeiro teste

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,30 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "mo-force-app-close.plugin",
+    "file": "plugins/mo-force-app-close/www/plugin.js",
+    "pluginId": "mo-force-app-close",
+    "clobbers": [
+      "MOForceAppClose"
+    ],
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "mo-force-app-close": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
